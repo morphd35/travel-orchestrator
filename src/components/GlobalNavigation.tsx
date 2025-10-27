0410931e357021889fd00f586f156908b6d74198
morphd35 <89456062+morphd35@users.noreply.github.com>
2025-10-27 16:24:03 -0500

    Fix Search Flights navigation to go to /search page instead of landing page

--- src/components/GlobalNavigation.tsx
+++ src/components/GlobalNavigation.tsx
@@ -46,7 +46,8 @@ export default function GlobalNavigation({ currentPage }: NavigationProps) {
             return;
         }
 
-        if (pathname === '/') setDetectedPage('search');
+        if (pathname === '/search') setDetectedPage('search');
+        else if (pathname === '/') setDetectedPage('home');
         else if (pathname === '/about') setDetectedPage('about');
         else if (pathname === '/watches') setDetectedPage('watches');
         else if (pathname.startsWith('/book')) setDetectedPage('book');
@@ -69,7 +70,7 @@ export default function GlobalNavigation({ currentPage }: NavigationProps) {
                     {/* Navigation Links */}
                     <div className="flex space-x-8">
                         <Link
-                            href="/"
+                            href="/search"
                             className={`px-3 py-2 rounded-md text-sm font-medium transition-colors ${detectedPage === 'search'
                                 ? 'bg-blue-100 text-blue-700'
                                 : 'text-gray-500 hover:text-gray-700 hover:bg-gray-100'
